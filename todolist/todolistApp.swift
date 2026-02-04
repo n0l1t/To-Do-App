6a0aa4ce0fe1d81542c76aa5016d0e016ca53046
n0lit <n0rthl1ght@yandex.ru>
2026-02-04 18:33:12 +0300

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Максим Фурсов on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
